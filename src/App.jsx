4a174ef05c6e14a6d923ddecf910ce3b40e24732
cuplprojects <shivom.chandrakala@gmail.com>
2025-05-15 15:10:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+        <h1 className="text-4xl font-bold text-blue-100">MeetX</h1>
+        <h1 className="text-2xl font-bold text-blue-100">Let's Connect With The World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
